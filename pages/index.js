5ac6898c61db1705c1b5d1322495917b36084e68
morafde <morafde@yahoo.com>
2022-08-27 19:35:48 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hedlamps!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          We are busy building a new and exciting site for you. <code>pages/index.js</code>
         </p>
       </main>
 
